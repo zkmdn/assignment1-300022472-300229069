a212bea4ccf9c9ae75c1a11b35e237ef63235664
Zaheer MacDonald <gmacd083@uottawa.ca>
2022-10-09 22:04:28 -0400

    changing test file

--- code_part2/Test.java
+++ code_part2/Test.java
@@ -1,10 +1,8 @@
 public class Test {
     public static void main (String[] args){
-        Employee newEmployee = new Employee("Faleao", 40, 15.50);
-        Address kingAddy = new Address("King Edward", 800, "K1N6N5");
-        Address queenAddy = new Address("Queen", 48, "K1P1N2");
-        newEmployee.addAddress(kingAddy);
-        newEmployee.addAddress(queenAddy);
+        Employee o1 = new Employee("Faleao", 40, 15.50);
+        o1.addAddress(new Address("King Edward", 800, "K1N6N5"));
+        o1.addAddress(new Address("Queen", 48, "K1P1N2"));
         
     }
 
