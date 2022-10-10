eba7baa203508095a22d863a74aa54b25b9419d3
Zaheer MacDonald <gmacd083@uottawa.ca>
2022-10-09 22:13:25 -0400

    changes to test

--- code_part2/Test.java
+++ code_part2/Test.java
@@ -3,7 +3,6 @@ public class Test {
         Employee o1 = new Employee("Faleao", 40, 15.50);
         o1.addAddress(new Address("King Edward", 800, "K1N6N5"));
         o1.addAddress(new Address("Queen", 48, "K1P1N2"));
-        
     }
 
 }
